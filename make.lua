20a00cf2add9e3ea0f5c5395be1bf54cf0397094
broma0 <121064585+broma0@users.noreply.github.com>
2026-04-16 22:49:09 -0400

    bump version, lock lua 5.1, update homepage

--- make.lua
+++ make.lua
@@ -1,10 +1,10 @@
 local env = {
   name = "santoku-mustache",
-  version = "0.0.14-1",
+  version = "0.0.15-1",
   license = "MIT",
   public = true,
   dependencies = {
-    "lua >= 5.1",
+    "lua == 5.1",
     "santoku >= 0.0.310-1",
   },
   cflags = {
@@ -29,7 +29,7 @@ local env = {
   }
 }
 
-env.homepage = "https://github.com/treadwelllane/lua-" .. env.name
+env.homepage = "https://github.com/birchpointswe/lua-" .. env.name
 env.tarball = env.name .. "-" .. env.version .. ".tar.gz"
 env.download = env.homepage .. "/releases/download/" .. env.version .. "/" .. env.tarball
 
