53166cf4211f332833d9561a93d0725b4e8d8700
broma0 <121064585+broma0@users.noreply.github.com>
2026-08-20 19:08:26 -0400

    make.lua: add configure
    make.lua
      + configure

--- make.lua
+++ make.lua
@@ -1,3 +1,19 @@
+local fs = require("santoku.fs")
+local vendor = require("santoku.make.vendor")
+
+local vendored = {
+  {
+    file = "deps/mustach/mustach-1.2.10.tar.gz",
+    url = "https://gitlab.com/jobol/mustach/-/archive/1.2.10/mustach-1.2.10.tar.gz",
+    sha256 = "95a2a351e748db9eeb98f40ba8bfbf010c1c6d2e725d31a3c7e602526d05bf90",
+  },
+}
+
+local include = {}
+for i = 1, #vendored do
+  include[i] = vendored[i].file
+end
+
 local env = {
   name = "santoku-mustache",
   version = "2.1.0-1",
@@ -7,12 +23,8 @@ local env = {
     "lua == 5.1",
     "santoku >= 2.0.0, < 3.0.0",
   },
-  vendor = {
-    {
-      file = "deps/mustach/mustach-1.2.10.tar.gz",
-      url = "https://gitlab.com/jobol/mustach/-/archive/1.2.10/mustach-1.2.10.tar.gz",
-      sha256 = "95a2a351e748db9eeb98f40ba8bfbf010c1c6d2e725d31a3c7e602526d05bf90",
-    },
+  rules = {
+    include = include,
   },
   cflags = {
     "-I$(shell luarocks show santoku --rock-dir)/include/",
@@ -28,7 +40,16 @@ local env = {
         "-sEMULATE_FUNCTION_POINTER_CASTS=1"
       }
     }
-  }
+  },
+  configure = function (submake, envs)
+    for i = 1, #vendored do
+      local v = vendored[i]
+      local dest = fs.join(envs.root.build_dir, v.file)
+      submake.target({ dest }, { "make.lua" }, function ()
+        vendor.fetch(v, dest)
+      end)
+    end
+  end,
 }
 
 env.homepage = "https://github.com/birchpointswe/lua-" .. env.name
